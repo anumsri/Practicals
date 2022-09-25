4809236666c2f5e46f8e9e6f297ae2436e4da0b9
ArpisittNumsri <you@example.com>
2022-09-25 16:18:18 +1000

    Added Temperature Conversion Functions

--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -8,13 +8,25 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = int(input("Enter Number of Items: "))
+
+
+def celsius_conversion():
+    global fahrenheit
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def farenheit_conversion():
+    global fahrenheit
+    fahrenheit = float(input("Farenheit: "))
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        celsius_conversion()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
-        fahrenheit = float(input("Farenheit: "))
+        farenheit_conversion()
         celsius = 5 / 9.0 * (fahrenheit - 32)
         print("Result: {:.2f} F".format(celsius))
     else:
