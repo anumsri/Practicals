c5c33ce37e7beaf5d1799375d7834b0a967b733d
ArpisittNumsri <you@example.com>
2022-09-25 16:14:16 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,4 +1,22 @@
-password = input("Enter Password: ")
-while len(password) < 6:
+MIN_LENGTH = 1
+MAX_LENGTH = 6
+
+
+def main():
+    password = get_password()
+    while len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        print("Invalid password")
+        password = get_password()
+    print_asterisk(password)
+
+
+def print_asterisk(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter Password: ")
-print("*" * len(password))
+    return password
+
+
+main()
