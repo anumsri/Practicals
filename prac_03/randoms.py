ca9e3ce148e81981a2a5084031534b7e63e54d7b
ArpisittNumsri <you@example.com>
2022-09-27 11:37:49 +1000

    Added random numbers

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+#
+# Smallest Number was 5, Largest number was 20
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+#
+# Only numbers 3, 5, 7 and 9 were seen
+# 4 Could not have been produced
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+#
+# 2.5 Would be the smallest and 5.5 would be the largest
+
+import random
+
+print(random.randint(1, 100))
+
