57529437a2efa573357e3e3c3bd187349aff8cf3
ArpisittNumsri <you@example.com>
2022-10-18 12:18:06 +1000

    Updated Wimbledon Function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,21 +1,16 @@
-"""
-CP1404/CP5632 Practical - Suggested Solution
-Wimbledon data-reading, processing and displaying
-"""
+
 FILENAME = "wimbledon.csv"
 INDEX_COUNTRY = 1
 INDEX_CHAMPION = 2
 
 
 def main():
-    """Read data file and print details about Wimbledon champions and countries."""
     records = get_records(FILENAME)
     champion_to_count, countries = process_records(records)
     display_results(champion_to_count, countries)
 
 
 def process_records(records):
-    """Create dictionary of champions and set of countries from records (list of lists)."""
     champion_to_count = {}
     countries = set()
     for record in records:
@@ -28,7 +23,6 @@ def process_records(records):
 
 
 def display_results(champion_to_count, countries):
-    """Display champions and countries"""
     print("Wimbledon Champions: ")
     for name, count in champion_to_count.items():
         print(name, count)
@@ -37,10 +31,9 @@ def display_results(champion_to_count, countries):
 
 
 def get_records(filename):
-    """Get records from file in list of lists form."""
     records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()  # Remove header
+        in_file.readline()
         for line in in_file:
             parts = line.strip().split(",")
             records.append(parts)
