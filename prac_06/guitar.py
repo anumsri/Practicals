158674e404f5031205945cbd66a6ac600d0b6aa8
ArpisittNumsri <you@example.com>
2022-11-02 21:14:20 +1000

    Completed Guitar Test Program

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,14 +1,12 @@
-
-
-
 CURRENT_YEAR = 2022
 VINTAGE_AGE = 50
+
 
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
-        self.year = cost
+        self.cost = cost
 
     def __str__(self):
         return f"{self.name}, ({self.year}) : ${self.cost:,.2f}"
